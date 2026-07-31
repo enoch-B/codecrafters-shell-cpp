ac9b72e3c47fc5ea5fbb62b360e13a99ebcb277d
enoch <henokt828@gmail.com>
2026-07-31 14:06:03 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ int main() {
       std::getline(std::cin, command);
       if(command.substr(0,5) == "echo "){
         std::cout<<command.substr(5)<<"\n"<< std::endl;
-    
+      }
        std::cout <<command<<": command not found" << std::endl;
 
   }
