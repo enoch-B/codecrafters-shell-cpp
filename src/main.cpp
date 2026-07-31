9019c9ef48f9cc7c21545c0181c4255fa99c5906
enoch <henokt828@gmail.com>
2026-07-31 17:24:22 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,41 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
-
-
-int main() {
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
- 
   std::string command;
   std::String paramaters;
 
-  while(true){
-      std::cout << "$ " << std::flush;
-      std::getline(std::cin, command);
-      if(command=="exit"){
-        break;
-      } else if(command.substr(0,5) == "echo "){
-        std::cout<<command.substr(5)<< std::endl;
-      }  else if (command == "type") {
+  while (true)
+  {
+    std::cout << "$ " << std::flush;
+    std::getline(std::cin, command);
+    if (command == "exit")
+    {
+      break;
+    }
+    else if (command.substr(0, 5) == "echo ")
+    {
+
+      std::cout << command.substr(5) << std::endl;
+    }
+    else if (command == "type")
+    {
+
       if (parameters == "echo" || parameters == "exit" || parameters == "type")
         std::cout << parameters << " is a shell builtin" << std::endl;
       else
         std::cout << parameters << ": not found" << std::endl;
-    } 
+    }
 
-      else{
-       std::cout <<command<<": command not found" << std::endl;
+    else
+    {
+      std::cout << command << ": command not found" << std::endl;
+    }
   }
-} 
 }
-
